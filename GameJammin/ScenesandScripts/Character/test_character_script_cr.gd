912b657381a77a4f9da574353ecd28df20096e6f
Romeo <foote08@gmail.com>
2024-07-24 15:55:22 -0400

    Player & Camera Controller
    Set up basic player controller + run/idle animations. Still working on fix to remove lag of looking in direction of player input. Still need to add to player controller for dousing/light animation. Completed Camera Controller. Added reference videos in comments at top of code.

---
+++ GameJammin/ScenesandScripts/Character/test_character_script_cr.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+#Player Controller reference video (https://www.youtube.com/watch?v=0T-FMkSru64)
+#Camera Controller reference video (https://www.youtube.com/watch?v=yVde3I3K7oo)
+
+#Player Controller Variables
+const Speed = 5.0
+var is_running = false
+@onready var animation_player = $Char2/AnimationPlayer
+@onready var character_model = $Char2
+
+#Camera Controller Variables
+@onready var camera_controller = $CameraController
+
+
+func _physics_process(_delta):
+	
+	#Player Controller
+	#Note, in our game map,movement is not the standard (-x. +x, -z, +z)
+	var input_dir = Input.get_vector("moveUp", "moveDown" , "moveRight" ,"moveLeft")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * Speed
+		velocity.z = direction.z * Speed 
+		#Set character model to look in direction of player input
+		character_model.look_at(direction + -position)
+		#If is_running does not equal false, then we set it to true & play Run animation
+		if !is_running:
+			is_running = true
+			animation_player.play("Run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, Speed)
+		velocity.z = move_toward(velocity.z, 0, Speed) 
+		
+		#If is_running does equal false, then we set it to false & play Idle animation
+		if is_running:
+			is_running = false
+			animation_player.play("Idle")
+
+	move_and_slide()
+	
+	#Camera Contoller
+	#Make camera controller position match the player's position via lerp
+	camera_controller.position = lerp(camera_controller.position, position, 0.10)
+	
