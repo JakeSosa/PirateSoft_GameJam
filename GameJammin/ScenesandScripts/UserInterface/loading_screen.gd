3093a2c3a7ebe76bdcadfe3df12eca65fd20b507
SJMLawrenceart <SJLawrence@student.fullsail.edu>
2024-07-26 10:36:44 -0500

    LoadingScreen

---
+++ GameJammin/ScenesandScripts/UserInterface/loading_screen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var progress = []
+var sceneName
+var sceneloadstatus = 0
+
+func _ready():
+	sceneName = "res://ScenesandScripts/Levels/level_1.tscn"
+	ResourceLoader.load_threaded_request(sceneName)
+
+
+
+func _process(delta):
+	sceneloadstatus = ResourceLoader.load_threaded_get_status(sceneName,progress)
+	$ColorRect/LoadPercent.text = str(floor(progress[0]*100)) + "%"
+	if sceneloadstatus == ResourceLoader.THREAD_LOAD_LOADED:
+		var newScene = ResourceLoader.load_threaded_get(sceneName)
+		get_tree().change_scene_to_file(sceneName)
