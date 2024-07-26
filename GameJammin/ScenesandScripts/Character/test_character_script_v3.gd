1ca741b6706f23f0a0c94539eb000cc40e274294
Romeo <foote08@gmail.com>
2024-07-26 17:15:36 -0400

    added comments to player script

--- GameJammin/ScenesandScripts/Character/test_character_script_v3.gd
+++ GameJammin/ScenesandScripts/Character/test_character_script_v3.gd
@@ -17,6 +17,7 @@ var isInArea : bool = false
 var change_torch_color : Color
 var torch_color
 var under_waterfall : = false
+
 func _ready():
 	torch_color = $Char2/Armature/Skeleton3D/BoneAttachment3D/MeshInstance3D/OmniLight3D.light_color
 
@@ -60,31 +61,29 @@ func player_camera():
 	camera_controller.position = lerp(camera_controller.position, position, 0.10)
 
 func torch_controller():
+	#If player is in area of brazier and presses E play Light animation
 	if isInArea == true && Input.is_action_just_pressed("lightTorch"):
 		is_lighting = true
 		torch.visible = true
 		$Char2/Armature/Skeleton3D/BoneAttachment3D/MeshInstance3D/OmniLight3D.light_color = change_torch_color
 		animation_player.play("Light")
 		$AnimationTimer.start()
-		
+	#If player presses Q play Douse animation	
 	if Input.is_action_just_pressed("douseTorch"):
 		is_dousing = true
 		$Char2/Armature/Skeleton3D/BoneAttachment3D/MeshInstance3D/OmniLight3D.light_color = Color.WHITE
 		animation_player.play("Douse")
 		$AnimationTimer.start()
-
-	
-
-	
+		
+#Emmited signals from brazier scene to pass color change value & if player in brazier area
 func _on_brazier_color_pass(colorChange):
 	change_torch_color = colorChange
-
 func _on_brazier_area_enter_check():
 	isInArea = true
-
 func _on_brazier_area_exit_check():
 	isInArea = false
 
+#Set Animation timer on player scene to make sure animation finishes before player moves
 func _on_animation_timer_timeout() -> void:
 	is_dousing = false
 	is_lighting = false
