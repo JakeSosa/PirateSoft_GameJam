c6b292fffc5161e9d3f98f94ea2d47519efcce84
Romeo <foote08@gmail.com>
2024-07-26 22:27:09 -0400

    Added animation for lever
    Added animation for lever

--- GameJammin/ScenesandScripts/Objects/Props/lever.gd
+++ GameJammin/ScenesandScripts/Objects/Props/lever.gd
@@ -2,14 +2,11 @@ extends StaticBody3D
 
 signal leverEnterCheck
 signal leverExitCheck
-# Called when the node enters the scene tree for the first time.
 
 
 func _on_lever_body_entered(body):
 	leverEnterCheck.emit()
-	print ("var changed")
-
 
 func _on_lever_body_exited(body):
 	leverExitCheck.emit()
-	print("var changed back")
+	
