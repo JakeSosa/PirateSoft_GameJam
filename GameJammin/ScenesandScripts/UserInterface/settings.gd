f830e3a74b7dec2a51445f38417dd44a00504178
SJMLawrenceart <SJLawrence@student.fullsail.edu>
2024-07-27 12:00:30 -0500

    Menus Update

--- GameJammin/ScenesandScripts/UserInterface/settings.gd
+++ GameJammin/ScenesandScripts/UserInterface/settings.gd
@@ -1,7 +1,12 @@
 extends Control
 
 
-
+func _on_music_volume_changed(value):
+	AudioServer.set_bus_volume_db(2,value)
 
 func _on_sfx_volume_changed(value):
 	AudioServer.set_bus_volume_db(1,value)
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://ScenesandScripts/UserInterface/main_menu.tscn")
