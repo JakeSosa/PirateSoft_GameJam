6acac2de1003078aa86a66a1f3a6129f33c676f0
IsraelKrzys <israelKrzys@gmail.com>
2024-07-29 16:43:33 -0400

    Sound vol fix, fire particle suck but exist

--- GameJammin/ScenesandScripts/Objects/Props/brazier_script_v2.gd
+++ GameJammin/ScenesandScripts/Objects/Props/brazier_script_v2.gd
@@ -9,9 +9,14 @@ extends StaticBody3D
 @export var player : CharacterBody3D
 #Set variable to define when player is near brazier
 var near_brazier := false
+@export var particle_color : Color
 
 func _ready():
 	$OmniLight3D.light_color = brazier_color
+	#print(brazier_color)
+	#$OmniLight3D/GPUParticles3D.process_material.resource_local_to_scene = true
+	#$OmniLight3D/GPUParticles3D.process_material.color = Color(1,0,0,1)
+	#print("PaticleColor ",$OmniLight3D/GPUParticles3D.process_material.color)
 	
 func _physics_process(delta: float) -> void:
 	brazier_controller()
--- GameJammin/ScenesandScripts/UserInterface/settings.gd
+++ GameJammin/ScenesandScripts/UserInterface/settings.gd
@@ -1,11 +1,8 @@
 extends Control
 
-
-func _on_music_volume_changed(value):
-	AudioServer.set_bus_volume_db(2,value)
-
-func _on_sfx_volume_changed(value):
-	AudioServer.set_bus_volume_db(1,value)
+func _process(delta):
+	AudioServer.set_bus_volume_db(2,linear_to_db($MarginContainer/VBoxContainer/MusicVolume.value))
+	AudioServer.set_bus_volume_db(1,linear_to_db($MarginContainer/VBoxContainer/SFXVolume.value))
 
 
 func _on_back_button_pressed():
