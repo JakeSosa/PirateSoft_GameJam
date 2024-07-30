619f2547a8f876e77019e0b405dc9e85f924ffdf
SJMLawrenceart <SJLawrence@student.fullsail.edu>
2024-07-29 20:39:51 -0500

    Menu Fixes

--- GameJammin/ScenesandScripts/UserInterface/settings.gd
+++ GameJammin/ScenesandScripts/UserInterface/settings.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@export var backSelected : Texture
+@export var backUnselected : Texture
+
 func _process(delta):
 	AudioServer.set_bus_volume_db(2,linear_to_db($MarginContainer/VBoxContainer/MusicVolume.value))
 	AudioServer.set_bus_volume_db(1,linear_to_db($MarginContainer/VBoxContainer/SFXVolume.value))
@@ -7,3 +10,11 @@ func _process(delta):
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://ScenesandScripts/UserInterface/main_menu.tscn")
+
+
+func _on_button_mouse_entered():
+	$PanelContainer/BackText.texture = backSelected
+
+
+func _on_button_mouse_exited():
+	$PanelContainer/BackText.texture = backUnselected
