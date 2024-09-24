930b8acd69db308c32f4eaf1c1ae37dce07bde06
Romeo <foote08@gmail.com>
2024-09-24 10:40:42 -0400

    updates to level 2

--- GameJammin/ScenesandScripts/Objects/Props/WaterFall_script.gd
+++ GameJammin/ScenesandScripts/Objects/Props/WaterFall_script.gd
@@ -2,23 +2,46 @@ extends Node3D
 
 #Set public variable that will act as a pointer to the player in level 1 scene
 @export var player : CharacterBody3D
-
+#Set variable for waterfall so that we can turn it on & off with lever pull
+@onready var waterfall = $"."
+#Set variable so we can define when player is under the waterfall
 var under_waterfall := false
-
+#Set variable for sound to play when player torch goes out under waterfall
 var sizzle = preload("res://Art/Sounds/SFX/PlayerSFX/Sizzle.mp3")
-
-func _on_waterfall_body_entered(body: Node3D) -> void:
-	if player.torch.visible == true:
-		MainMenuMusic.play_SFX(sizzle)
-	under_waterfall = true
-	player.torch.visible = false
-	player.torch.light_color = Color.WHITE
 	
+#When the player goes under waterfall, douse player torch and play sizzle sound
+func _on_waterfall_body_entered(body: Node3D) -> void:
+	if waterfall.visible == true:
+		if player.torch.visible == true:
+			MainMenuMusic.play_SFX(sizzle)
+		under_waterfall = true
+		player.torch.visible = false
+		player.torch.light_color = Color.WHITE
 	
 func _on_waterfall_body_exited(body: Node3D) -> void:
 	under_waterfall = false
 	
+#For WaterFall_2 (near green brazier)
+#WaterFall_2
 func _on_lever_kill_waterfall() -> void:
-	$".".queue_free()
-	print("waterfall turned off")
-	
+	waterfall.visible = false
+#WaterFall_2	
+func _on_lever_revive_waterfall() -> void:
+	waterfall.visible = true
+
+#For WaterFall_3 (near red brazier)
+#WaterFall_3
+func _on_lever_revive_waterfall_3() -> void:
+	waterfall.visible = false
+#WaterFall_3
+func _on_lever_kill_waterfall_3() -> void:
+	waterfall.visible = true
+
+
+#For BigWaterFall_2 (near purple door)
+#BigWaterFall_2 
+func _on_lever_2_kill_waterfall() -> void:
+	waterfall.visible = false
+#BigWaterFall_2 
+func _on_lever_2_revive_waterfall() -> void:
+	waterfall.visible = true
--- GameJammin/ScenesandScripts/Objects/Props/lever.gd
+++ GameJammin/ScenesandScripts/Objects/Props/lever.gd
@@ -11,12 +11,16 @@ extends StaticBody3D
 @onready var audio_player = $LeverSFX
 #Set variable to define when player is near lever
 var near_lever := false
-var interactable = true
+var lever_down = false
+var lever_up = true
 #Send signal to specific waterfall in scene to turn it off
 signal kill_waterfall
+#Send signal to specific waterfall in scent to turn it on
+signal revive_waterfall
 
 func _ready() -> void:
-	interactable = true
+	lever_down = false
+	lever_up = true
 	
 func _physics_process(delta: float) -> void:
 	lever_controller()
@@ -28,13 +32,25 @@ func _on_lever_body_exited(body):
 	near_lever = false
 	
 func lever_controller():
-	if interactable == true:
-		#If player is near lever & presses E, play Pull animation
-		if near_lever == true && Input.is_action_just_pressed("interact"):
+	#If player is near lever & presses E, play Pull animation
+	if near_lever == true && Input.is_action_just_pressed("interact"):
+		#If current position of lever is pulled up
+		if lever_up == true && lever_down == false:
+			#Play animation to pull lever down and set new downward position for lever
+			animation_player.play("PullAgain")
+			audio_player.play()
+			lever_down = true
+			lever_up = false
+			#Emit signal from lever script to waterfall script to turn off waterfall 2
+			kill_waterfall.emit()
+			
+		elif lever_up == false && lever_down == true:
+			#Play animation to pull lever up and set new upward position for lever
 			animation_player.play("Pull")
 			audio_player.play()
-			#Emit signal from lever script to waterfall script
-			kill_waterfall.emit()
-			#After the player interacts with the lever, change interactable to false
-			interactable = false
+			lever_down = false
+			lever_up = true
+			#Emit signal from lever script to waterfall script to turn off waterfall 3
+			revive_waterfall.emit()
+		
 	
